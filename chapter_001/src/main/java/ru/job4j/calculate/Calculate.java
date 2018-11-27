f43d526053c605ab14ad9f26030fb107e0ff7746
Ajderka <emage.haf@gmail.com>
2018-11-27 23:39:16 +0200

    JUnit [#91039]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,17 +2,25 @@ package ru.job4j.calculate;
 
 /**
  *Class Calculate решение задачи части 001 урок1.
- *@author khayredinov
+ *@author Ayder Khayredinov (emage.haf@gmail.com)
  *@since 26.11.2018
  *@version 1
 */
 
-public class Calculate {
-	/**
-	* Конструктор, вывод строки в консоль.
-	* @param args - args.
-	*/
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
+	public class Calculate {
+		/**
+		* Конструктор, вывод строки в консоль.
+		* @param args - args.
+		*/
+		public static void main(String[] args) {
+			System.out.println("Hello world");
+		}
+		/**
+		* Method echo.
+		* @param name Ayder Khayredinov (emage.haf@gmail.com)
+		* @return Echo plus your name.
+		*/
+		public String echo(String name) {
+			return "Echo, echo, echo : " + name;
+		}
+	}
