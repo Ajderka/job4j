e9aedcdafef304859d1e659ae67e08b230965671
Ajderka <emage.haf@gmail.com>
2018-12-05 19:08:45 +0200

    4.4. Максимум из трех чисел[#91004]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,20 +1,33 @@
 package ru.job4j.max;
 
 /**
- * Class  4.2. Максимум из двух чисел[#91003]
+ * Class  4.4. Максимум из трех чисел[#91004]
+ *
  * @author Ayder Khayredinov (emage.haf@gmail.com)
- * @since 03.12.2018
  * @version 1
+ * @since 05.12.2018
  */
 public class Max {
 
     /**
      * Method max - Максимум из двух чисел.
-     * @param first - первый аргумент выражения.
+     *
+     * @param first  - первый аргумент выражения.
      * @param second - второй аргумент выражения.
      */
     public int max(int first, int second) {
         return first < second ? second : first;
+    }
+
+    /**
+     * Method maxThree - Максимум из трех чисел.
+     *
+     * @param first  - первый аргумент выражения.
+     * @param second - второй аргумент выражения.
+     * @param third  - второй аргумент выражения.
+     */
+    public int maxThree(int first, int second, int third) {
+        return this.max(this.max(first, second), third);
     }
 
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,24 +1,27 @@
 package ru.job4j.max;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * Test.
+ *
  * @author Ayder Khayredinov (emage.haf@gmail.com)
- * @since 03.12.2018
  * @version 1
+ * @since 05.12.2018
  */
 public class MaxTest {
 
     /**
-     * Test Максимум из двух чисел.
+     * Test Максимум из трех чисел.
      */
     @Test
-    public void whenFirstLessSecond() {
+    public void whenFirstLessSecondLessThree() {
         Max maxim = new Max();
-        int result = maxim.max(1, 2);
-        assertThat(result, is(2));
+        int result = maxim.maxThree(1, 2, 3);
+        assertThat(result, is(3));
+
     }
 }
