474860e0d7a29121cafe7d62f34860df8c88c7ce
Ajderka <emage.haf@gmail.com>
2018-12-10 23:13:58 +0200

    6.4. Слово начинается с ...[#91018]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+/**
+ * Class  Массив заполнен true или false[#91019].
+ *
+ * @author Ayder Khayredinov (emage.haf@gmail.com).
+ * @version 1.
+ * @since 10.12.2018.
+ */
+public class ArrayChar {
+
+    /**
+     * ссылка на массив типо char.
+     */
+    private char[] data;
+
+    /**
+     * конструктор принимает строку и преобразует ее в массив data типо char.
+     */
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     *
+     * @param prefix префикс.
+     * @return если слово начинается с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int index = 0; index < prefix.length(); index++) {
+            if (data[index] != value[index]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Ayder Khayredinov (emage.haf@gmail.com).
+ * @version 1.
+ * @since 10.12.2018.
+ */
+public class ArrayCharTest {
+
+    /**
+     * Test вводит строку "Hello", и строку "He", проверяет результат true.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Test вводит строку "Hello", и строку "Hi", проверяет результат false.
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
