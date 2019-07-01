2ab01ab2b8b6ea2dc7f773386476fcf8dae43478
Айдер <emage.haf@gmail.com>
2019-07-01 15:10:16 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#148077]

---
+++ chapter_005/src/main/java/ru/job4j/collectionpro/list/Node.java
@@ -0,0 +1,62 @@
+package ru.job4j.collectionpro.list;
+
+/**
+ * @author Ayder Khayredinov (emage.haf@gmail.com).
+ * @version 1.
+ * @since 1.07.2019.
+ */
+public class Node<E> {
+
+    E value;
+    Node<E> next;
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    /**
+     * Метод поиска наличия цыкличности.
+     *
+     * @param first элемент с которого будет начат анализ цыклиности.
+     */
+    boolean hasCycle(Node first) {
+        boolean result = false;
+        Node slow, fast;
+        slow = fast = first;
+
+        if (first != null) {
+            while (true) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (fast == null) {
+                    result = false;
+                    break;
+                }
+                if (slow == fast) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+}
+
+/*
+Node first = new Node(1);
+Node two = new Node(2);
+Node third = new Node(3);
+Node four = new Node(4);
+
+first.next = two;
+two.next = third;
+third.next = four;
+four.next = first;
+
+Написать алгоритм определяющий, что список содержит замыкания.
+
+boolean hasCycle(Node first);
+
+Обратите внимание, что список может быть замкнут и в середине. К примеру, 3-й узел будет ссылаться на 2-й узел. Определение зацикленности необходимо реализовать путем прохода по узлам, без использования коллекций.
+ */
---
+++ chapter_005/src/test/java/ru/job4j/collectionpro/list/NodeTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.collectionpro.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Ayder Khayredinov (emage.haf@gmail.com).
+ * @version 1.
+ * @since 1.07.2019.
+ */
+public class NodeTest {
+    private Node first;
+    private Node second;
+    private Node third;
+    private Node four;
+
+    @Before
+    public void beforeTest() {
+        first = new Node(1);
+        second = new Node(2);
+        third = new Node(3);
+        four = new Node(4);
+    }
+
+    @Test
+    public void whenWeDoFullCycle() {
+        first.next = second;
+        second.next = third;
+        third.next = four;
+        four.next = first;
+        assertThat(first.hasCycle(first), is(true));
+    }
+
+    @Test
+    public void whenDoSmallCycle() {
+        first.next = second;
+        second.next = third;
+        third.next = four;
+        four.next = second;
+        assertThat(first.hasCycle(first), is(true));
+    }
+
+    @Test
+    public void whenWeDidNotHaveCycle() {
+        first.next = second;
+        second.next = third;
+        third.next = four;
+        assertThat(first.hasCycle(first), is(false));
+    }
+}
